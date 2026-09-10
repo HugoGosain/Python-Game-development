08bace6cebaaf99cc1c8517cef30249735e5762d
HugoGosain <hugogosain@gmail.com>
2026-09-10 18:39:18 +0200

    Add files via upload

--- Lesson 2-alien shooter/win.py
+++ Lesson 2-alien shooter/win.py
@@ -5,7 +5,7 @@ WIDTH = 600
 HEIGHT = 600
 message = ""
 score = 0
-game_started = False
+game_started = "Start"
 button = Actor("startbutton")
 alien = Actor("alien")
 
@@ -16,7 +16,7 @@ def position():
 def draw():
     global game_started
     screen.clear()
-    if not game_started:
+    if game_started == "Start":
         screen.draw.text(
             "Alien Game",
             center=(300,180),
@@ -24,30 +24,41 @@ def draw():
         )
         button.pos = (300,300)
         button.draw()
-    else:
+    elif game_started == "Game":
         alien.draw()
         screen.draw.text(message,center =(300,10), fontsize= 20)
         screen.draw.text(f"score = {score}",center =(30,10), fontsize= 20)
+    else:
+        wintext = "!YOU WIN!"
+        screen.draw.text(wintext,center =(300,300), fontsize= 150)
 
 
 def on_mouse_down(pos):
     global message   
     global score  
     global game_started
-    if not game_started:
+    if game_started == "Start":
         if button.collidepoint(pos):
-            game_started = True
+            game_started = "Game"
             score = 0
             message = ""
             position()
-    else:    
+    elif game_started == "Game":    
         if alien.collidepoint(pos):
             position()
             message = "hit!"
             score += 1
+            if score == 30:
+                game_started = "End"
         else:
             message = "miss!"
-            score -= 1
+            score -= 5
+    else:
+        game_started = "End"
+
+
+
+    
 
 
 def update():
